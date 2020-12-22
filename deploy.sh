0e1b4b275b8f0f6fd16200634042448391412ba2
Pasquale Congiusti <pasquale.congiusti@gmail.com>
2020-12-22 11:56:06 +0100

    Adding tag script variable

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,11 @@
 VERSION="latest"
 
+if [ -z "$1" ] ; then
+    echo "No argument supplied, building the docker image as LATEST"
+else
+    VERSION="$1"
+    echo "Building the docker image as $VERSION"
+fi
+
 docker tag jabberwocky-ui:$VERSION  squakez/jbw-ui:$VERSION
 docker push squakez/jbw-ui:$VERSION
--- make.sh
+++ make.sh
@@ -1,1 +1,10 @@
-docker build -t jabberwocky-ui:latest -f ./build/Dockerfile .
+VERSION="latest"
+
+if [ -z "$1" ] ; then
+    echo "No argument supplied, building the docker image as LATEST"
+else
+    VERSION="$1"
+    echo "Building the docker image as $VERSION"
+fi
+
+docker build -t jabberwocky-ui:$VERSION -f ./build/Dockerfile .
